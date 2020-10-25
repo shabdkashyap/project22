0101faa9767ae38ac5be8746fcc1a9a1a21b2666
shabdkashyap <71210343+shabdkashyap@users.noreply.github.com>
2020-10-25 19:52:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -56,7 +56,7 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-	Matter.Body.setStatic( packageSprite , false);
+	Matter.Body.setStatic( packageBody , false);
   }
 }
 
